baf31d324f08ed82b45e7d0eea7d8aa60d4735de
imran5152 <afridkhan9371@gmail.com>
2026-02-11 19:03:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 10000
 
 app.get('/', (req, res) => {
-  res.send('Aarush Technologies | Codingwale Vaijapur | Node.js App Deployed Successfully | Version 2.0.0 🚀
+  res.send('Aarush Technologies | Codingwale Vaijapur | Node.js App Deployed Successfully | Version 3.0.0 🚀
 ')
 })
 
