67b7fb6bf6e840b19a8dc9ec871022084300c411
imran5152 <afridkhan9371@gmail.com>
2026-02-11 20:15:55 +0530

    Update version in response message to 2.0.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 10000
 
 app.get('/', (req, res) => {
-  res.send('Aarush Technologies | Codingwale Vaijapur | Node.js App Deployed Successfully | Version 1.0.0 🚀')
+  res.send('Aarush Technologies | Codingwale Vaijapur | Node.js App Deployed Successfully | Version 2.0.0 🚀')
 })
 
 app.listen(port,'0.0.0.0', () => {
